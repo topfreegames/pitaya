ecb7789016991369646555bf4d670fcbd978adac
Andre Hahn <andre.hahn@tfgco.com>
2018-07-26 19:53:35 -0300

    Trying to fix rtd tables

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ release = u''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+        'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
