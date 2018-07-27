ac51fd184c9b13e035521faa38281e9ce5e400bb
Andre <andrehpereira@gmail.com>
2018-07-26 23:09:54 -0300

    Changing doc tables to rst

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,6 @@ release = u''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
