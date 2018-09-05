fbe396b8967430722c9654b84bd675965de36f62
Rafael Leite <rafael.leite@tfgco.com>
2018-09-05 19:32:20 -0300

    Fix validator when argument is nil

---
+++ defaultpipelines/default_struct_validator_test.go
@@ -0,0 +1,62 @@
+package defaultpipelines
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TestingStruct struct {
+	Name         string `json:"name"`
+	Email        string `json:"email" validate:"email"`
+	SoftCurrency int    `json:"softCurrency" validate:"gte=0,lte=1000"`
+	HardCurrency int    `json:"hardCurrency" validate:"gte=0,lte=200"`
+}
+
+func TestDefaultValidator(t *testing.T) {
+	validator := &DefaultValidator{}
+
+	var tableTest = map[string]struct {
+		s          *TestingStruct
+		shouldFail bool
+	}{
+		"validation_error": {
+			s: &TestingStruct{
+				Email: "notvalid",
+			},
+			shouldFail: true,
+		},
+		"validation_success": {
+			s: &TestingStruct{
+				Name:         "foo",
+				Email:        "foo@tfgco.com",
+				SoftCurrency: 100,
+				HardCurrency: 10,
+			},
+		},
+		"validate_nil_object": {
+			s:          nil,
+			shouldFail: false,
+		},
+	}
+
+	for tname, tbl := range tableTest {
+		t.Run(tname, func(t *testing.T) {
+			var err error
+			if tbl.s == nil {
+				_, err = validator.Validate(context.Background(), nil)
+			} else {
+				_, err = validator.Validate(context.Background(), tbl.s)
+			}
+
+			if tbl.shouldFail {
+				assert.Error(t, err)
+			} else {
+				fmt.Println(err)
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
