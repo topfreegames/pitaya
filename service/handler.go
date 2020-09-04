1461dd746d82e0bd97b7276f99f94b8a5e1e965d
colstuwjx <colstuwjx@gmail.com>
2020-09-04 14:35:16 +0800

    Fix log print uid fmt string %d -> %s or %v.

--- service/handler.go
+++ service/handler.go
@@ -176,7 +176,7 @@ func (h *HandlerService) Handle(conn acceptor.PlayerConn) {
 	// guarantee agent related resource is destroyed
 	defer func() {
 		a.Session.Close()
-		logger.Log.Debugf("Session read goroutine exit, SessionID=%d, UID=%d", a.Session.ID(), a.Session.UID())
+		logger.Log.Debugf("Session read goroutine exit, SessionID=%d, UID=%s", a.Session.ID(), a.Session.UID())
 	}()
 
 	for {
