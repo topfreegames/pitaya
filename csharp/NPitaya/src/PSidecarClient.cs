f0226cc3650b732ed96dfad96b8b563825e85ca4
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2021-07-29 14:39:35 -0300

    Rollback target to netstandard2.0 This will remove support for UDS in windows for now

--- csharp/NPitaya/src/PSidecarClient.cs
+++ csharp/NPitaya/src/PSidecarClient.cs
@@ -1,37 +1,10 @@
 using System;
 using Grpc.Core;
 using NPitaya.Protos;
-using System.Net.Http;
-using System.Net;
-using System.Net.Sockets;
-using Grpc.Net.Client;
 
 namespace NPitaya
 {
 
-    public class UnixDomainSocketConnectionFactory
-    {
-        private readonly EndPoint _endPoint;
-        public UnixDomainSocketConnectionFactory(EndPoint endPoint)
-        {
-            _endPoint = endPoint;
-        }
-        public async System.Threading.Tasks.ValueTask<System.IO.Stream> ConnectAsync(SocketsHttpConnectionContext _,
-            System.Threading.CancellationToken cancellationToken = default)
-        {
-            var socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
-            try
-            {
-                await socket.ConnectAsync(_endPoint, cancellationToken).ConfigureAwait(false);
-                return new NetworkStream(socket, true);
-            }
-            catch
-            {
-                socket.Dispose();
-                throw;
-            }
-        }
-    }
     public partial class PitayaCluster
     {
         public static Sidecar.SidecarClient InitializeSidecarClient(string sidecarListenAddr, NPitaya.Protos.Server server, bool debug = false)
@@ -40,18 +13,7 @@ namespace NPitaya
             GrpcEnvironment.SetThreadPoolSize(Environment.ProcessorCount);
             // TODO tls stuff
 
-            var udsEndPoint = new UnixDomainSocketEndPoint(sidecarListenAddr);
-            var connectionFactory = new UnixDomainSocketConnectionFactory(udsEndPoint);
-            var socketsHttpHandler = new SocketsHttpHandler
-            {
-                ConnectCallback = connectionFactory.ConnectAsync
-            };
-
-            var channel = GrpcChannel.ForAddress("http://localhost", new GrpcChannelOptions
-            {
-                HttpHandler = socketsHttpHandler
-            });
-
+            Channel channel = new Channel(sidecarListenAddr, ChannelCredentials.Insecure);
 
             var client = new Sidecar.SidecarClient(channel);
             var req = new StartPitayaRequest { Config = server, DebugLog = debug };
