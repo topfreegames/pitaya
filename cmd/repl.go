c4c3eccacbf0f3e043ee523304ddc4dec4cf0818
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2024-07-29 13:05:03 -0300

    remove invalid char

--- cmd/repl.go
+++ cmd/repl.go
@@ -1,5 +1,5 @@
 /*
-Copyright � 2021 Wildlife Studios
+Copyright 2024 Wildlife Studios
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
