aa9c77eb6297533e7891bcd4953b41aa4f149c41
Rafael da Fonseca <rafael.fonseca@wildlifestudios.com>
2025-11-21 13:10:50 +0000

    match v2 implementation

--- pkg/cluster/nats_rpc_common.go
+++ pkg/cluster/nats_rpc_common.go
@@ -23,7 +23,6 @@ package cluster
 import (
 	"fmt"
 	"os"
-	"syscall"
 	"time"
 
 	nats "github.com/nats-io/nats.go"
@@ -34,13 +33,54 @@ func getChannel(serverType, serverID string) string {
 	return fmt.Sprintf("pitaya/servers/%s/%s", serverType, serverID)
 }
 
+func drainAndClose(nc *nats.Conn) error {
+	if nc == nil {
+		return nil
+	}
+	// If connection is already closed, just return
+	if nc.IsClosed() {
+		return nil
+	}
+	// Drain connection (this will flush any pending messages and prevent new ones)
+	err := nc.Drain()
+	if err != nil {
+		logger.Log.Warnf("error draining nats connection: %v", err)
+		// Even if drain fails, try to close (but only if not already closed)
+		if !nc.IsClosed() {
+			nc.Close()
+		}
+		return err
+	}
+
+	// Wait for drain to complete with timeout
+	timeout := time.After(5 * time.Second)
+	ticker := time.NewTicker(100 * time.Millisecond)
+	defer ticker.Stop()
+
+	for nc.IsDraining() {
+		select {
+		case <-ticker.C:
+			continue
+		case <-timeout:
+			logger.Log.Warn("drain timeout exceeded, forcing close")
+			if !nc.IsClosed() {
+				nc.Close()
+			}
+			return fmt.Errorf("drain timeout exceeded")
+		}
+	}
+
+	// Close will happen automatically after drain completes
+	return nil
+}
+
 func setupNatsConn(connectString string, appDieChan chan bool, options ...nats.Option) (*nats.Conn, error) {
 	connectedCh := make(chan bool)
 	initialConnectErrorCh := make(chan error)
 	natsOptions := append(
 		options,
-		nats.DisconnectErrHandler(func(_ *nats.Conn, err error) {
-			logger.Log.Warnf("disconnected from nats! Reason: %q\n", err)
+		nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
+			logger.Log.Warnf("disconnected from nats (%s)! Reason: %v", nc.ConnectedAddr(), err)
 		}),
 		nats.ReconnectHandler(func(nc *nats.Conn) {
 			logger.Log.Warnf("reconnected to nats server %s with address %s in cluster %s!", nc.ConnectedServerName(), nc.ConnectedAddr(), nc.ConnectedClusterName())
@@ -53,19 +93,52 @@ func setupNatsConn(connectString string, appDieChan chan bool, options ...nats.O
 			}
 
 			logger.Log.Errorf("nats connection closed. reason: %q", nc.LastError())
+
+			// If connection was never successfully established, prioritize initialConnectErrorCh
+			// to allow setupNatsConn to return quickly with an error
+			wasConnected := nc.ConnectedAddr() != ""
+
+			if !wasConnected {
+				// During initial connection, send error to initialConnectErrorCh first
+				select {
+				case initialConnectErrorCh <- nc.LastError():
+					return
+				default:
+					// If channel is not ready, fall through to appDieChan handling
+				}
+			}
+
 			if appDieChan != nil {
 				select {
 				case appDieChan <- true:
 					return
 				case initialConnectErrorCh <- nc.LastError():
 					logger.Log.Warnf("appDieChan not ready, sending error in initialConnectCh")
+					return
 				default:
-					logger.Log.Warnf("no termination channel available, sending SIGTERM to app")
-					err := syscall.Kill(os.Getpid(), syscall.SIGTERM)
+					logger.Log.Warnf("no termination channel available, sending termination signal to app")
+
+					p, err := os.FindProcess(os.Getpid())
 					if err != nil {
-						logger.Log.Errorf("could not kill the application via SIGTERM, exiting", err)
+						logger.Log.Errorf("could not find current process: %v", err)
 						os.Exit(1)
 					}
+
+					// On Windows, Signal() with Interrupt works
+					// On Unix-like systems, this is equivalent to SIGINT
+					err = p.Signal(os.Interrupt)
+					if err != nil {
+						logger.Log.Errorf("could not send interrupt signal to the application: %v", err)
+						os.Exit(1)
+					}
+				}
+			} else if !wasConnected {
+				// If no appDieChan and connection was never established, try initialConnectErrorCh again
+				select {
+				case initialConnectErrorCh <- nc.LastError():
+					return
+				default:
+					// Channel not ready, but we've already logged the error
 				}
 			}
 		}),
@@ -78,8 +151,13 @@ func setupNatsConn(connectString string, appDieChan chan bool, options ...nats.O
 				logger.Log.Errorf(err.Error())
 			}
 		}),
-		nats.ConnectHandler(func(*nats.Conn) {
-			connectedCh <- true
+		nats.ConnectHandler(func(nc *nats.Conn) {
+			logger.Log.Infof("connected to nats on %s", nc.ConnectedAddr())
+			select {
+			case connectedCh <- true:
+			default:
+				// Channel not ready, but connection is established
+			}
 		}),
 	)
 
@@ -104,8 +182,16 @@ func setupNatsConn(connectString string, appDieChan chan bool, options ...nats.O
 	case <-connectedCh:
 		return nc, nil
 	case err := <-initialConnectErrorCh:
+		drainErr := drainAndClose(nc)
+		if drainErr != nil {
+			logger.Log.Warnf("failed to drain and close: %s", drainErr)
+		}
 		return nil, err
 	case <-time.After(maxConnTimeout * 2):
+		drainErr := drainAndClose(nc)
+		if drainErr != nil {
+			logger.Log.Warnf("failed to drain and close: %s", drainErr)
+		}
 		return nil, fmt.Errorf("timeout setting up nats connection")
 	}
 }
